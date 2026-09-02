6e9e2cf556d8378062c76ad95221d3438e4fb818
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2026-09-02 10:01:10 +0800

    Code updates 10:01am

--- clinic-management-ui/src/app/features/users/containers/login/login.component.ts
+++ clinic-management-ui/src/app/features/users/containers/login/login.component.ts
@@ -1,45 +1,9 @@
-import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
-import { CardModule } from 'primeng/card';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-login',
-    standalone: true,
-    schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    imports: [
-        CommonModule,
-        ReactiveFormsModule,
-        ButtonModule,
-        InputTextModule,
-        CardModule
-    ],
+    standalone: false,
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
-    loginForm!: FormGroup;
-
-    constructor(private formBuilder: FormBuilder, private router: Router) { }
-
-    ngOnInit(): void {
-        this.loginForm = this.formBuilder.group({
-            username: ['', [Validators.required]],
-            password: ['', [Validators.required]]
-        });
-    }
-
-    onSubmit(): void {
-        if (this.loginForm.valid) {
-            const formValues = this.loginForm.value;
-            alert(JSON.stringify(formValues, null, 2));
-        }
-    }
-
-    navigateToRegister(): void {
-        this.router.navigate(['/register']);
-    }
-}
+export class LoginComponent { }
