f8551596e1dd3481533afc909d6d22c9b0305538
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2026-09-07 00:41:28 +0800

    Sept 7 updates

--- clinic-management-ui/src/app/app.module.ts
+++ clinic-management-ui/src/app/app.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
-import { environment } from '../enviroments/enviroment';
 
 @NgModule({
     declarations: [AppComponent],
@@ -12,13 +9,6 @@ import { environment } from '../enviroments/enviroment';
         BrowserModule,
         AppRoutingModule
     ],
-    providers: [
-        providePrimeNG({
-            theme: {
-                preset: Aura
-            },
-            license: environment.primeNgLicense
-        })],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
